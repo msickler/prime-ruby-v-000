95a873693cf77e49688439003c6e27935f352098
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-29 12:23:51 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,12 +2,9 @@
 def prime?(integer)
 array = (1..integer-1).to_a
 array.any? do |i|
-  if integer < 2
+  if integer < 2 && integer % i == 0
     false
-  elsif array.none? do |i|
-    integer % i == 0
-  end
-    false
+    
   else
     true
   end
