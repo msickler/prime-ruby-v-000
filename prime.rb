ad1927ec32ac07a0fdc8f532d68f15228ce045f7
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-29 12:10:07 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,14 @@
 # Add  code here!
 def prime?(integer)
 array = (1..10).to_a
-array.any? { |i| i > 1  }
+array.any? do |i| 
+  if i < 1
+    false
+  elsif i % 2 = 0
+    false
+  else 
+    true
+  end    
+end
 
 end
